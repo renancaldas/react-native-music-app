6ad920bfe757dd7261ee0be6b55c7a540d80ee80
Renan Caldas <renan.calda@gmail.com>
2020-08-08 04:37:32 -0300

    Added slider and timestamps (momentjs)

--- app/Components/MusicPlayer/MusicPlayer.js
+++ app/Components/MusicPlayer/MusicPlayer.js
@@ -1,16 +1,45 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import TrackPlayer from 'react-native-track-player';
-import {prettyLog} from '../../helpers/helpers';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import {errorLog, reactLog, prettyLog} from '../../helpers/helpers';
+import IconAntDesign from 'react-native-vector-icons/AntDesign';
+import IconFeather from 'react-native-vector-icons/Feather';
+import Slider from '@react-native-community/slider';
+import moment from 'moment';
 
 class ProgressBar extends TrackPlayer.ProgressComponent {
+  onSlidingStart(data) {
+    prettyLog('onSlidingStart', JSON.stringify(data));
+  }
+
+  onSlidingComplete(seconds) {
+    prettyLog('onSlidingComplete', JSON.stringify(seconds));
+    TrackPlayer.seekTo(seconds);
+  }
+
+  onValueChange(seconds) {
+    prettyLog('onValueChange', JSON.stringify(seconds));
+    TrackPlayer.seekTo(seconds);
+  }
+
+  getTimeFromSeconds(seconds) {
+    return moment(new Date()).startOf('day').seconds(seconds).format('mm:ss');
+  }
+
   render() {
     return (
-      <View>
-        <Text>Position: {this.state.position}</Text>
-        <Text>Progress: {this.getProgress()}</Text>
-        <Text>BufferedProgress: {this.getBufferedProgress()}</Text>
+      <View style={styles.progressBarTimers}>
+        <Text>{this.getTimeFromSeconds(this.state.position)}</Text>
+        <Slider
+          style={{width: '70%'}}
+          value={this.state.position}
+          minimumValue={0}
+          maximumValue={this.state.duration}
+          onSlidingStart={this.onSlidingStart}
+          onSlidingComplete={this.onSlidingComplete}
+          onValueChange={this.onValueChange}
+        />
+        <Text>{this.getTimeFromSeconds(this.state.duration)}</Text>
       </View>
     );
   }
@@ -23,14 +52,14 @@ class MusicPlayer extends Component {
   };
 
   componentDidMount() {
-    prettyLog('componentDidMount', 'Called');
+    reactLog('componentDidMount()', 'Called');
     // Adds an event handler for the playback-track-changed event
     this.onTrackChange = TrackPlayer.addEventListener(
       'playback-track-changed',
       async (data) => {
         const track = await TrackPlayer.getTrack(data.nextTrack);
-        prettyLog('playback-track-changed', track);
-        this.setState({track});
+        prettyLog('playback-track-changed', JSON.stringify(track));
+        this.setState({track: track.id});
       },
     );
 
@@ -49,11 +78,14 @@ class MusicPlayer extends Component {
   }
 
   componentWillUnmount() {
-    prettyLog('componentWillUnmount');
+    reactLog('componentWillUnmount()');
 
     // Removes the event handler
     this.onTrackChange.remove();
-    this.setState({playerState: null});
+    this.onStateChange.remove();
+
+    // Destroy player
+    TrackPlayer.destroy();
 
     // Log spacing
     console.log('');
@@ -62,33 +94,33 @@ class MusicPlayer extends Component {
   componentDidUpdate(provProps, prevState) {
     const prevStateLog = `\nPrevious State: ${JSON.stringify(prevState)}`;
     const currStateLog = `\nCurrent State: ${JSON.stringify(this.state)}`;
-    prettyLog('componentDidUpdate', `${prevStateLog} ${currStateLog}`);
+    reactLog('componentDidUpdate()', `${prevStateLog} ${currStateLog}`);
 
     const isPlayerReady = !prevState.playerState && this.state.playerState;
     if (isPlayerReady) {
-      prettyLog('componentDidUpdate', `isPlayerReady`);
+      prettyLog('componentDidUpdate()', `isPlayerReady`);
 
       this.addTrack({
-        id: 'patriota', // Must be a string, required
-        title: 'Patriota',
-        artist: 'Augusto Lima',
-        album: 'Gonna Patriotar',
+        id: 'Honors_Over', // Must be a string, required
+        title: 'Over',
+        artist: 'Honors',
+        album: 'SuicideSheep',
         genre: 'Rock',
         date: '2014-05-20T07:00:00+00:00', // RFC 3339
-        url: require('../../assets/patriota.mp4'), // Load media from the app bundle
-        artwork: require('../../assets/patriota.png'), // Load artwork from the app bundle
+        url: require('../../assets/Honors_Over.mp4'), // Load media from the app bundle
+        artwork: require('../../assets/Honors_Over.png'), // Load artwork from the app bundle
       });
     }
   }
 
   initPlayer() {
-    prettyLog('initPlayer', `Called`);
+    prettyLog('initPlayer()', `Called`);
     const self = this;
 
     TrackPlayer.setupPlayer().then(() => {
       TrackPlayer.getState().then((playerState) => {
         prettyLog(
-          'initPlayer',
+          'initPlayer()',
           `Player initialized: ${JSON.stringify(playerState)}`,
         );
         this.setState({playerState});
@@ -97,44 +129,54 @@ class MusicPlayer extends Component {
   }
 
   addTrack(track) {
-    prettyLog('addTrack', `Called`);
+    prettyLog('addTrack()', `Called`);
+    prettyLog('addTrack()', `track: ${JSON.stringify(track)}`);
 
-    TrackPlayer.add([track]).then(async () => {
-      prettyLog('addTrack', `Track added: ${JSON.stringify(track)}`);
+    TrackPlayer.add([track])
+      .then(async () => {
+        prettyLog('addTrack'(), `Track added: ${JSON.stringify(track)}`);
 
-      const currentTrack = await TrackPlayer.getCurrentTrack();
-      prettyLog('addTrack', `currentTrack: ${JSON.stringify(currentTrack)}`);
+        const currentTrack = await TrackPlayer.getCurrentTrack();
+        prettyLog(
+          'addTrack()',
+          `currentTrack: ${JSON.stringify(currentTrack)}`,
+        );
 
-      this.setState({track: currentTrack});
-    });
+        this.setState({track: currentTrack});
+      })
+      .catch((err) => errorLog('TrackPlayer.add', err));
   }
 
   onPressPlay() {
-    prettyLog('onPressPlay', `Called`);
+    prettyLog('onPressPlay()', `Called`);
     TrackPlayer.play();
   }
 
   onPressPause() {
-    prettyLog('onPressPause', `Called`);
+    prettyLog('onPressPause()', `Called`);
     TrackPlayer.pause();
   }
 
   onPressStop() {
-    prettyLog('onPressStop', `Called`);
+    prettyLog('onPressStop()', `Called`);
     TrackPlayer.stop();
   }
+
   render() {
+    reactLog('render()', `this.state.track: ${this.state.track}`);
     return (
       <View style={styles.container}>
-        <View style={styles.header}>
-          <Text>{this.state.track && this.state.track ? this.state.track : 'N/A'}</Text>
-        </View>
-
         <View style={styles.content}>
           <Image
             style={styles.image}
-            source={require('../../assets/patriota.png')}
+            source={require('../../assets/Honors_Over.png')}
           />
+        </View>
+
+        <View style={styles.header}>
+          <Text>
+            {this.state.track && this.state.track ? this.state.track : 'N/A'}
+          </Text>
         </View>
 
         <View style={styles.progressBar}>
@@ -142,8 +184,15 @@ class MusicPlayer extends Component {
         </View>
 
         <View style={styles.playerControls}>
-          <Icon
-            name="backward"
+          <IconFeather
+            name="shuffle"
+            size={20}
+            color="black"
+            onPress={this.onPressPlay}
+          />
+
+          <IconAntDesign
+            name="stepbackward"
             size={30}
             color="black"
             onPress={this.onPressPlay}
@@ -151,20 +200,27 @@ class MusicPlayer extends Component {
 
           {['paused', 'ready'].includes(this.state.playerState) ? (
             <TouchableOpacity onPress={this.onPressPlay}>
-              <Icon name="play" size={30} color="black" />
+              <IconAntDesign name="play" size={40} color="black" />
             </TouchableOpacity>
           ) : (
             <TouchableOpacity onPress={this.onPressPause}>
-              <Icon name="pause" size={30} color="black" />
+              <IconAntDesign name="pause" size={40} color="black" />
             </TouchableOpacity>
           )}
 
-          {/* <Icon name="stop" size={30} color="black"  onPress={this.onPressStop}/> */}
-          <Icon
-            name="forward"
+          {/* <IconAntDesign name="stop" size={30} color="black"  onPress={this.onPressStop}/> */}
+          <IconAntDesign
+            name="stepforward"
             size={30}
             color="black"
             onPress={this.onPressStop}
+          />
+
+          <IconFeather
+            name="repeat"
+            size={20}
+            color="black"
+            onPress={this.onPressPlay}
           />
         </View>
       </View>
@@ -174,36 +230,42 @@ class MusicPlayer extends Component {
 
 const styles = StyleSheet.create({
   container: {},
+  content: {
+    height: 300,
+    marginBottom: 20,
+    borderColor: 'black',
+    // borderWidth: 1,
+  },
+  image: {
+    height: '100%',
+    width: '100%',
+  },
   header: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
     height: 30,
     borderColor: 'black',
-    borderWidth: 1,
-  },
-  content: {
-    height: 300,
-    borderColor: 'black',
-    borderWidth: 1,
-    marginBottom: 30,
-  },
-  image: {
-    height: '100%',
-    width: '100%',
+    // borderWidth: 1,
   },
   progressBar: {
     height: 50,
+    marginBottom: 20,
     borderColor: 'black',
-    borderWidth: 1,
-    marginBottom: 30,
+    // borderWidth: 1,
+  },
+  progressBarTimers: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   playerControls: {
-    height: 35,
+    height: 50,
+    alignItems: 'center',
     flexDirection: 'row',
     justifyContent: 'space-around',
     borderColor: 'black',
-    borderWidth: 1,
+    // borderWidth: 1,
   },
 });
 
--- app/helpers/helpers.js
+++ app/helpers/helpers.js
@@ -1,5 +1,11 @@
-const titleStyle = 'background: blue; color: white;';
+export const reactLog = (title, message) => {
+  console.log(`%c` + title + '%c ' + message, 'background: blue; color: white;', null);
+};
 
 export const prettyLog = (title, message) => {
-  console.log(`%c` + title + '%c ' + message, titleStyle, null);
+  console.log(`%c` + title + '%c ' + message, 'background: yellow; color: black;', null);
+};
+
+export const errorLog = (title, message) => {
+  console.log(`%c` + title + '%c ' + message, 'background: red; color: white;', null);
 };
