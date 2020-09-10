22cc3517448d082868e13c93a700701c477dad9f
Renan Caldas <renan.calda@gmail.com>
2020-09-10 16:48:27 -0300

    Removed merge leftovers

--- app/Views/Player.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-import MusicPlayer from '../Components/MusicPlayer/MusicPlayer';
-
-const Player = () => {
-  return <MusicPlayer />;
-};
-
-export default Player;
